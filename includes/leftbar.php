e8493220e9ba2051e63a6c09eeb603ec665e9836
RuchiraHansanaBandara <ruchirabandara67@gmail.com>
2025-01-24 11:21:23 +0530

    add manage teacher file

--- includes/leftbar.php
+++ includes/leftbar.php
@@ -49,7 +49,7 @@
                                         <a href="#"><i class="fa fa-users"></i> <span>Teachers</span> <i class="fa fa-angle-right arrow"></i></a>
                                         <ul class="child-nav">
                                             <li><a href="add-teachers.php"><i class="fa fa-bars"></i> <span>Add Teachers</span></a></li>
-                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Manage Teachers</span></a></li>
+                                            <li><a href="manage-teachers.php"><i class="fa fa fa-server"></i> <span>Manage Teachers</span></a></li>
                                            
                                         </ul>
                                     </li>
