3d8872bf68345a325265f1644dfb55c89c27b84a
RuchiraHansanaBandara <ruchirabandara67@gmail.com>
2025-03-09 11:38:19 +0530

    add cube animation for find

--- find-result.php
+++ find-result.php
@@ -14,12 +14,24 @@ include('includes/config.php');?>
     <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
     <link rel="stylesheet" href="css/icheck/skins/flat/blue.css" >
     <link rel="stylesheet" href="css/main.css" media="screen" >
+    <link rel="stylesheet" href="css/cube.css" media="screen">
     <script src="js/modernizr/modernizr.min.js"></script>
 </head>
 <body class="">
     <div class="main-wrapper">
 
-        <div class="login-bg-color bg-black-300">
+        <!-- Cube container to fill the full background -->
+        <div class="cube-container">
+            <!-- Create a large number of cubes dynamically -->
+            <?php
+            // Generate 300 cubes for better coverage
+            for ($i = 0; $i < 300; $i++) {
+                echo '<div class="cube" style="top:' . rand(0, 100) . '%; left:' . rand(0, 100) . '%; animation-delay:' . rand(0, 5) . 's;"></div>';
+            }
+            ?>
+        </div>
+
+        <div class="login-bg-color bg-black-500">
             <div class="row">
                 <div class="col-md-4 col-md-offset-4">
                     <div class="panel login-box">
@@ -30,42 +42,39 @@ include('includes/config.php');?>
                         </div>
                         <div class="panel-body p-20">
 
-                        
-
                             <form action="result.php" method="post">
                                 <div class="form-group">
                                     <label for="rollid">Enter your Roll Id</label>
                                     <input type="text" class="form-control" id="rollid" placeholder="Enter Your Roll Id" autocomplete="off" name="rollid">
                                 </div>
-                            <div class="form-group">
-                                                    <label for="default" class="col-sm-2 control-label">Class</label>
-<select name="class" class="form-control" id="default" required="required">
-<option value="">Select Class</option>
-<?php $sql = "SELECT * from tblclasses";
-$query = $dbh->prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{   ?>
-<option value="<?php echo htmlentities($result->id); ?>"><?php echo htmlentities($result->ClassName); ?>&nbsp; Section-<?php echo htmlentities($result->Section); ?></option>
-<?php }} ?>
-</select>
-</div>
-
+                                <div class="form-group">
+                                    <label for="default" class="col-sm-2 control-label">Class</label>
+                                    <select name="class" class="form-control" id="default" required="required">
+                                        <option value="">Select Class</option>
+                                        <?php 
+                                        $sql = "SELECT * from tblclasses";
+                                        $query = $dbh->prepare($sql);
+                                        $query->execute();
+                                        $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                        if($query->rowCount() > 0) {
+                                            foreach($results as $result) { ?>
+                                                <option value="<?php echo htmlentities($result->id); ?>">
+                                                    <?php echo htmlentities($result->ClassName); ?>&nbsp; Section-<?php echo htmlentities($result->Section); ?>
+                                                </option>
+                                        <?php }} ?>
+                                    </select>
+                                </div>
 
                                 <div class="form-group mt-20">
                                     <div class="">
-                                    
                                         <button type="submit" class="btn btn-success btn-labeled pull-right">Search<span class="btn-label btn-label-right"><i class="fa fa-check"></i></span></button>
                                         <div class="clearfix"></div>
                                     </div>
                                 </div>
 
-                                    <div class="col-sm-6">
-                                                            <a href="index.php">Back to Home</a>
-                                                        </div>
+                                <div class="col-sm-6">
+                                    <a href="index.php">Back to Home</a>
+                                </div>
                             </form>
 
                             <hr>
@@ -105,6 +114,5 @@ foreach($results as $result)
         });
     </script>
 
-    <!-- ========== ADD custom.js FILE BELOW WITH YOUR CHANGES ========== -->
 </body>
 </html>
